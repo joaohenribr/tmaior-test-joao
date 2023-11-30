06c0faf479c81bdba1d15dee1721e04608d10637
João Henrique <joaohb28@gmail.com>
2023-11-30 18:31:32 -0300

    Adicionado a conexão ao banco de dados

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 var express = require('express');
-
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 var server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
-   });
+});
 
 app.use(express.static(__dirname));
 
 var mongoose = require('mongoose');
+var dbUrl = 'mongodb+srv://admin:EJwLcbTY1znnVu00@cluster0.dpjsz7z.mongodb.net/?retryWrites=true&w=majority'
 
-var dbUrl = 'mongodb+srv://username:password@ds257981.mongodb.net:57981/simple-chat'
-
-mongoose.connect(dbUrl , (err) => { 
-    console.log('mongodb connected',err);
- })
+mongoose
+    .connect(dbUrl)
+    .then((success) => console.log('Conectado ao MongoDB com sucesso'))
+    .catch((err) => console.log('Erro ao conectar ao MongoDB', err));
 
 var Message = mongoose.model('Message',{ name : String, message : String})
 
+app.get('/messages', (req, res) => {
+    Message.find({},(err, messages)=> {
+        res.send(messages);
+    })
+})
+
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) =>{
+        if(err)
+            sendStatus(500);
+        res.sendStatus(200);
+    })
+})
