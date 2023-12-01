b3d3818a59ae224b5c14c9c6360d71fc12bfcf15
João Henrique <joaohb28@gmail.com>
2023-12-01 12:05:13 -0300

    Aplicação funcionando corretamente

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+app.use(express.static(__dirname));
 
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log('server is running on port', server.address().port);
 });
 
-app.use(express.static(__dirname));
-
-var mongoose = require('mongoose');
 var dbUrl = 'mongodb+srv://admin:EJwLcbTY1znnVu00@cluster0.dpjsz7z.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose
@@ -25,11 +28,37 @@ app.get('/messages', (req, res) => {
     })
 })
 
-app.post('/messages', (req, res) => {
-    var message = new Message(req.body);
-    message.save((err) =>{
-        if(err)
-            sendStatus(500);
-        res.sendStatus(200);
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name: user},(err, messages)=> {
+      res.send(messages);
     })
+})
+
+app.post('/messages', async (req, res) => {
+    try{
+      var message = new Message(req.body);
+  
+      var savedMessage = await message.save()
+        console.log('saved');
+  
+      var censored = await Message.findOne({message:'badword'});
+        if(censored)
+          await Message.remove({_id: censored.id})
+        else
+          io.emit('message', req.body);
+        res.sendStatus(200);
+    }
+    catch (error){
+      res.sendStatus(500);
+      return console.log('error',error);
+    }
+    finally{
+      console.log('Message Posted')
+    }
+  
+  })
+
+io.on('connection', () =>{
+    console.log('a user is connected')
 })
